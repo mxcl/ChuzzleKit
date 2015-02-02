3b7241c1084f8e5055694e1ea4e5e26f33a04dd0
Max Howell <mxcl@me.com>
2015-02-02 15:17:10 -0600

    Support Carthage

---
+++ ChuzzleKit.h
@@ -0,0 +1,8 @@
+#import <ChuzzleKit/Chuzzle.h>
+#import <Foundation/Foundation.h>
+
+//! Project version number for ChuzzleKit.
+FOUNDATION_EXPORT double ChuzzleKitVersionNumber;
+
+//! Project version string for ChuzzleKit.
+FOUNDATION_EXPORT const unsigned char ChuzzleKitVersionString[];
---
+++ Tests/ChuzzleKitTests.m
@@ -0,0 +1,31 @@
+#import <ChuzzleKit/ChuzzleKit.h>
+@import XCTest;
+
+
+@interface ChuzzleKitTests : XCTestCase
+@end
+
+@implementation ChuzzleKitTests
+
+- (void)test_everything {
+    id b = @[@{@"a": @"", @2: @"", @3: @[], @4: [NSNull null]}];
+    id a = @[NSNull.null, @"\t", @{@1: @"   ", @2: b}, @1].chuzzle;
+    XCTAssertEqualObjects(a, @[@1]);
+}
+
+- (void)test_no_chuzzle {
+    NSDictionary *a =  @{@1: @"1", @2: @"2"};
+    id b = a.chuzzle;
+    XCTAssertEqualObjects(a, b);
+}
+
+- (void)test_another_test {
+    NSDictionary *d = @{@1: @"", @2: @"  2  "}.chuzzle;
+    XCTAssertEqualObjects(d, @{@2: @"2"});
+}
+- (void)test_nsobject {
+    id o = [NSObject new];
+    XCTAssertEqual(o, [o chuzzle]);
+}
+
+@end
